9429d5a7bcf5746cc99cdef865198598ef1700aa
kao98 <kao98@kaonet-fr.net>
2016-03-12 14:27:04 +0100

    Adding routes and pages for portfolio categories

--- src/js/appRoutes.jsx
+++ src/js/appRoutes.jsx
@@ -4,14 +4,20 @@ import Router, {Route, Redirect} from 'react-router';
 
 import Page from './components/page.jsx';
 import Home from './components/home.jsx';
-import Portfolio from './components/portfolio.jsx';
 import Viewer from './components/viewer.jsx';
+import PortfolioHousing from './components/portfolio/housing.jsx';
+import PortfolioMiscellaneous from './components/portfolio/miscellaneous.jsx';
+import PortfolioPublicService from './components/portfolio/public-service.jsx';
 
 export default (
         <Route path="/" handler={Page}>
             <Redirect from="/" to="/home" />
             <Route name="home" handler={Home} />
-            <Route name="portfolio" handler={Portfolio} />
+            <Route path="portfolio">
+                <Route name="logements" handler={PortfolioHousing} />
+                <Route name="autres" handler={PortfolioMiscellaneous} />
+                <Route name="services-publiques" handler={PortfolioPublicService} />
+            </Route>
             <Route name="viewer" path="/view/:pictureUrl" handler={Viewer} />
         </Route>
 );
