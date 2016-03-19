85439f715b09572733659598b0d9aef11747a2fb
kao98 <kao98@kaonet-fr.net>
2016-03-19 10:34:11 +0100

    Adding references page and route

--- src/js/appRoutes.jsx
+++ src/js/appRoutes.jsx
@@ -7,6 +7,7 @@ import Home from './components/home.jsx';
 import PortfolioHousing from './components/portfolio/housing.jsx';
 import PortfolioMiscellaneous from './components/portfolio/miscellaneous.jsx';
 import PortfolioPublicService from './components/portfolio/public-service.jsx';
+import References from './components/references.jsx';
 
 export default (
     <Route path="/" component={Page}>
@@ -16,6 +17,7 @@ export default (
             <Route path="autres" component={PortfolioMiscellaneous} />
             <Route path="services-publiques" component={PortfolioPublicService} />
         </Route>
+        <Route path="references" component={References} />
     </Route>
 );
 
---
+++ src/js/components/references.jsx
@@ -0,0 +1,17 @@
+/* references.jsx */
+
+import React from 'react';
+
+export default React.createClass({
+
+    render() {
+
+        return (
+            <div id='references-page'>
+                Réééé-fééé-rences!!! \o/
+            </div>
+        );
+    },
+
+
+});
